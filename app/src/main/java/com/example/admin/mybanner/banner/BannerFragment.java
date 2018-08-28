3c0e6a159dd9ca3bc4ec968ebbf6c020c8f12845
yqli18 <yqli18@iflytek.com>
2018-08-28 13:48:00 +0800

    修改

--- app/src/main/java/com/example/admin/mybanner/banner/BannerFragment.java
+++ app/src/main/java/com/example/admin/mybanner/banner/BannerFragment.java
@@ -1,6 +1,8 @@
 package com.example.admin.mybanner.banner;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -24,31 +26,32 @@ import com.example.admin.mybanner.R;
  * create by yqli on 2018/8/10
  */
 public class BannerFragment extends Fragment {
-    private final static String VIEWTYPE ="VIEWTYPE";
+    private final static String VIEWTYPE = "VIEWTYPE";
     private int viewType;//0 图片，1视频，2 文字
-    private final static String VIEWRESOUSEID="VIEWRESOUSEID";
+    private final static String VIEWRESOUSEID = "VIEWRESOUSEID";
     private int viewResouceID;
-    private final static String VIEWRESOUSESTR="VIEWRESOUSESTR";
+    private final static String VIEWRESOUSESTR = "VIEWRESOUSESTR";
     private String viewResouceStr;
-    public final static int bannerImage=0;
-    public final static int bannerVideoView=1;
-    public final static int bannerTextView=2;
+    public final static int bannerImage = 0;
+    public final static int bannerVideoView = 1;
+    public final static int bannerTextView = 2;
     private VideoView vv;
     ConstraintLayout cl;
 
-    public static BannerFragment newInstance(int viewType,int viewResouceID) {
+    public static BannerFragment newInstance(int viewType, int viewResouceID) {
         BannerFragment fragment = new BannerFragment();
         Bundle bundle = new Bundle();
         bundle.putInt(VIEWTYPE, viewType);
-        bundle.putInt(VIEWRESOUSEID,viewResouceID);
+        bundle.putInt(VIEWRESOUSEID, viewResouceID);
         fragment.setArguments(bundle);
         return fragment;
     }
-    public static BannerFragment newInstance(int viewType,String viewResouceStr) {
+
+    public static BannerFragment newInstance(int viewType, String viewResouceStr) {
         BannerFragment fragment = new BannerFragment();
         Bundle bundle = new Bundle();
         bundle.putInt(VIEWTYPE, viewType);
-        bundle.putString(VIEWRESOUSESTR,viewResouceStr);
+        bundle.putString(VIEWRESOUSESTR, viewResouceStr);
         fragment.setArguments(bundle);
         return fragment;
     }
@@ -58,42 +61,55 @@ public class BannerFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             viewType = getArguments().getInt(VIEWTYPE);
-            viewResouceID=getArguments().getInt(VIEWRESOUSEID);
-            viewResouceStr=getArguments().getString(VIEWRESOUSESTR);
+            viewResouceID = getArguments().getInt(VIEWRESOUSEID);
+            viewResouceStr = getArguments().getString(VIEWRESOUSESTR);
         }
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_banner, container, false);
-        cl=view.findViewById(R.id.cl);
-        Context context=getContext();
+        View view = inflater.inflate(R.layout.fragment_banner, container, false);
+        cl = view.findViewById(R.id.cl);
+        Context context = getContext();
         LinearLayout.LayoutParams layoutParams;
-        layoutParams =new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
-        switch (viewType){
+        switch (viewType) {
             case bannerImage:
-                ImageView iv=new ImageView(context);
-                cl.addView(iv,layoutParams);
-                if (!TextUtils.isEmpty(viewResouceStr)){
+                ImageView iv = new ImageView(context);
+                cl.addView(iv, layoutParams);
+                if (!TextUtils.isEmpty(viewResouceStr)) {
                     Glide.with(context).load(viewResouceStr).into(iv);
                 }
-                if (viewResouceID!=0){
+                if (viewResouceID != 0) {
                     Glide.with(context).load(R.drawable.what_1).into(iv);
                 }
                 break;
             case bannerVideoView:
                 vv = new VideoView(context);
-                cl.addView(vv,layoutParams);
-                if (!TextUtils.isEmpty(viewResouceStr)){
-                  vv.setVideoPath(viewResouceStr);
+                cl.addView(vv, layoutParams);
+                if (!TextUtils.isEmpty(viewResouceStr)) {
+                    vv.setVideoPath(viewResouceStr);
                 }
+                vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mp) {
+                        mp.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+                            @Override
+                            public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                                if (what == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START)
+                                    vv.setBackgroundColor(Color.TRANSPARENT);
+                                return true;
+                            }
+                        });
+                    }
+                });
                 break;
             case bannerTextView:
-                TextView tv=new TextView(context);
-                cl.addView(tv,layoutParams);
-                if (!TextUtils.isEmpty(viewResouceStr)){
+                TextView tv = new TextView(context);
+                cl.addView(tv, layoutParams);
+                if (!TextUtils.isEmpty(viewResouceStr)) {
                     tv.setText(viewResouceStr);
                 }
                 break;
@@ -104,7 +120,7 @@ public class BannerFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        if (vv!=null){
+        if (vv != null) {
             vv.start();
         }
     }
@@ -112,7 +128,7 @@ public class BannerFragment extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
-        if (vv!=null){
+        if (vv != null) {
             vv.stopPlayback();
         }
     }
